352da79171bd50daa7f2a05a0f4b4467eed1f3d8
Shakil00077 <shakilshehjaad@gmail.com>
2022-03-21 16:02:16 +0600

    added event listener

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -2,13 +2,21 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    const {name, price, id} = props.product
+    const { name, price, id } = props.product
+
+    const addToCart = (id) => {
+        console.log('product added', id);
+    }
+
+    // const addToCartWithPara = () => addToCart(id)
+
     return (
         <div className='product'>
             <h4>Product Name: {name}</h4>
             <h6>Price: {price}</h6>
             <p>Id: {id}</p>
-            <button>Add to Cart</button>
+            <button onClick={() => addToCart(id)}>Add to Cart</button>
+            {/* <button onClick={addToCartWithPara}>Add to Cart2</button> */}
         </div>
     );
 };
